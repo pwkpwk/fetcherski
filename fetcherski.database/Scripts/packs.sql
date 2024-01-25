21e0c15b07d4ac9c406f74c042c53141965d4f42
Pavel Karpenko <pwkpwk@hotmail.com>
2024-01-25 15:55:55 -0800

    Script the linked packs/pack_items tables

--- fetcherski.database/Scripts/packs.sql
+++ fetcherski.database/Scripts/packs.sql
@@ -1,8 +1,53 @@
-﻿create table public."packs" (
-    id UUID primary key default gen_random_uuid(),
-    created TIMESTAMP not null,
-    sequential_id INT8 not null generated always as identity,
-    name string(50) not null,
+﻿create table public."packs"
+(
+    id            UUID primary key default gen_random_uuid(),
+    sequential_id INT8      not null generated always as identity,
+    created       TIMESTAMP not null,
+    name          string(50) not null,
     unique index "packs_sequential_id_index" (sequential_id asc),
-    index "packs_created_index" (created asc) 
-)
+    index "packs_created_index" (created asc)
+);
+
+drop table public."pack_contents";
+
+create table public."pack_contents"
+(
+    id            UUID primary key default gen_random_uuid(),
+    sequential_id INT8 not null generated always as identity,
+    pack_id       UUID not null,
+    description   STRING(100) not null,
+    unique index "pack_contents_id_index" (id),
+    unique index "pack_contents_index" (pack_id, id),
+    constraint "pack_contents_pack_ref" foreign key (pack_id) references "packs" (id),
+    family "pack_contents_items" (pack_id, sequential_id, id)
+);
+
+create view "pack_contents_forward" as
+select "pack_contents".id            "id",
+       "packs".created               "created",
+       "pack_contents".sequential_id "sequential_id",
+       "pack_contents".description   "description",
+       "packs".id                    "pack_id",
+       "packs".name                  "pack_name"
+from "packs"
+         join "pack_contents" on ("packs".id = "pack_contents".pack_id
+    )
+order by created, sequential_id;
+
+create view "pack_contents_reverse" as
+select "pack_contents".id            "id",
+       "packs".created               "created",
+       "pack_contents".sequential_id "sequential_id",
+       "pack_contents".description   "description",
+       "packs".id                    "pack_id",
+       "packs".name                  "pack_name"
+from "packs"
+         join "pack_contents" on ("packs".id = "pack_contents".pack_id
+    )
+order by created desc, sequential_id desc;
+
+select *
+from "pack_contents_forward";
+
+select *
+from "pack_contents_reverse";
