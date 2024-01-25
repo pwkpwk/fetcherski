d6f34cb93a06f0efa80ea5d8438c02f2c89b6cc6
Pavel Karpenko <pwkpwk@hotmail.com>
2024-01-25 00:15:41 -0800

    Create 'packs' table

---
+++ fetcherski.database/Scripts/packs.sql
@@ -0,0 +1,8 @@
+﻿create table public."packs" (
+    id UUID primary key default gen_random_uuid(),
+    created TIMESTAMP not null,
+    sequential_id INT8 not null generated always as identity,
+    name string(50) not null,
+    unique index "packs_sequential_id_index" (sequential_id asc),
+    index "packs_created_index" (created asc) 
+)
