791b5c52814bc8e31e53672c1929d64530b83e40
Pavel Karpenko <pwkpwk@hotmail.com>
2024-01-24 23:52:34 -0800

    Add data source and SQL scripts

---
+++ fetcherski.database/Scripts/fetcherski.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE public.looseitems
+(
+    id            UUID      NOT NULL DEFAULT gen_random_uuid(),
+    sequential_id INT8      NOT NULL GENERATED ALWAYS AS IDENTITY,
+    created       TIMESTAMP NOT NULL,
+    description   STRING(100) NOT NULL,
+    CONSTRAINT looseitems_pkey PRIMARY KEY (id ASC),
+    UNIQUE INDEX looseitems_sequential_id_index (sequential_id ASC),
+    INDEX         looseitems_created_index (created ASC)
+)
