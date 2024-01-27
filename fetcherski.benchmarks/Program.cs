86c9e7da112a5431b69194cfbaa6950417d92245
Pavel Karpenko <pwkpwk@hotmail.com>
2024-01-26 22:14:36 -0800

    Add benchmarks project

---
+++ fetcherski.benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using BenchmarkDotNet.Running;
+
+namespace fetcherski.benchmarks;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
+    }
+}
---
+++ fetcherski.benchmarks/tools/UnfoldAsyncSequence.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using fetcherski.tools;
+
+namespace fetcherski.benchmarks.tools;
+
+public class UnfoldAsyncSequence
+{
+    [Params(50, 250)] public int SequenceLength;
+
+    [Benchmark(Description = "Return a completed task")]
+    public async Task ReturnTask()
+    {
+        var sequence =
+            AsyncSequences.Unfold((state, _) => Task.FromResult((state + 1, state + 1, state < SequenceLength)), 0);
+
+        await foreach (var n in sequence)
+        {
+            // Do nothing            
+        }
+    }
+
+    [Benchmark(Description = "Compiler writes async method")]
+    public async Task AsyncKeyword()
+    {
+        var sequence = AsyncSequences.Unfold(async (state, _) => (state + 1, state + 1, state < SequenceLength), 0);
+
+        await foreach (var n in sequence)
+        {
+            // Do nothing            
+        }
+    }
+}
