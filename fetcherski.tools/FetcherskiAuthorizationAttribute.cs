3d516a983c9d39a338ae2779c34e17c35a757948
Pavel Karpenko <pwkpwk@hotmail.com>
2024-06-30 10:08:43 -0700

    Log authorization

--- fetcherski.tools/FetcherskiAuthorizationAttribute.cs
+++ fetcherski.tools/FetcherskiAuthorizationAttribute.cs
@@ -1,22 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 
 namespace fetcherski.tools;
 
+/// <summary>
+/// Attribute that may be applied to methods of API controllers to process actions routed to the methods.
+/// </summary>
+/// <param name="Name">Optional name of the action that the attribute authorizes. If not specified,
+/// ASP.Net derives the name from the controller method, removing "Async" if the method returns a task.</param>
 public class FetcherskiAuthorizationAttribute(string? Name = null) : ActionFilterAttribute
 {
+    private static readonly EventId BadRequestEventId = new(400, "Authorization");
+    private static readonly EventId UnauthorizedEventId = new(401, "Authorization");
+    private static readonly EventId AuthorizedEventId = new(200, "Authorization");
+
     public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
-        var authorization = (IAuthorization)context.HttpContext.RequestServices.GetService(typeof(IAuthorization))!;
-        string actionName = Name ?? context.ActionDescriptor.RouteValues["action"]!; 
+        var authorization = GetService<IAuthorization>(context);
+        var logger = GetService<ILogger<FetcherskiAuthorizationAttribute>>(context);
 
-        if (! await authorization.AuthorizeAsync(actionName, context.HttpContext.RequestAborted))
+        string? actionName = Name ?? context.ActionDescriptor.RouteValues["action"];
+
+        if (string.IsNullOrWhiteSpace(actionName))
         {
-            context.Result = new UnauthorizedResult();
-            return;
+            logger.LogError(BadRequestEventId, "Unspecified action name");
+            context.Result = new BadRequestResult();
         }
-        
-        // Base implementation routes the action to the selected controller
-        await base.OnActionExecutionAsync(context, next);
+        else if (!await authorization.AuthorizeAsync(actionName, context.HttpContext.RequestAborted))
+        {
+            logger.LogError(UnauthorizedEventId, "Failed to authorize action '{actionName}'", actionName);
+            context.Result = new UnauthorizedResult();
+        }
+        else
+        {
+            logger.LogTrace(AuthorizedEventId, "Authorized action '{actionName}'", actionName);
+            // Base implementation routes the action to the selected controller
+            await base.OnActionExecutionAsync(context, next);
+        }
     }
+
+    private static T GetService<T>(ActionExecutingContext context) where T : class =>
+        (context.HttpContext.RequestServices.GetService(typeof(T)) as T)!;
 }
