ca1764b31f24a1e0d67d6d6a327b7e31f62fd104
Pavel Karpenko <pwkpwk@hotmail.com>
2024-07-16 22:49:43 -0700

    Expand comments

--- fetcherski.controllers.fs/SupplementalController.fs
+++ fetcherski.controllers.fs/SupplementalController.fs
@@ -7,17 +7,25 @@ open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Logging
 open fetcherski.tools
 
+/// <summary>
 /// ASP.Net API controller written in F# only to demonstrate how to write asynchronous action methods.
+/// 'KerbungleRequirement' policy applies the <see cref="KerbungleRequirement"/> requirement that has one
+/// parameter, <see cref="KerbungleRequirement.KerbungleTokenRequired"/>, that tells the authorization handler
+/// to check the authenticated user for the authenticated identity of the type 'Kerbungle', added by
+/// <see cref="KerbungleAuthentication"/> if the Authorization header hes a Kerbungle token.
+/// </summary>
+/// <seealso cref="KerbungleAuthentication"/>
+/// <seealso cref="KerbungleAuthorization"/>
 [<Route("api/mafia")>]
 [<Authorize(nameof KerbungleRequirement)>]
 type SupplementalController(logger: ILogger<SupplementalController>) =
     inherit Controller()
 
     // Parentheses are needed because all C# functions in F# take arguments as tuples
-    static let PlopEventId = EventId(1, nameof SupplementalController)
+    static let MakeDonEventId = EventId(1, nameof SupplementalController)
 
     let rec makeDonAsync (donName: string) (delay: int) ct =
-        logger.LogTrace(PlopEventId, "{member} | donName={donName}, delay={delay}", nameof makeDonAsync, donName, delay)
+        logger.LogTrace(MakeDonEventId, "{member} | donName={donName}, delay={delay}", nameof makeDonAsync, donName, delay)
 
         task {
             do! Task.Delay(delay, ct)
